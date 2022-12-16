b94e1a0278afea1583f1e3a30644f43af718a611
Najaf <najaf_babayev@yahoo.com>
2022-12-16 17:05:29 +0400

    movies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appName: 'angular-movies',
+  webDir: 'dist/angular-movies',
+  bundledWebRuntime: false
+};
+
+export default config;
